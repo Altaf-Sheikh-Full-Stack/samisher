59efb4fea0b0de2843216ddcf8e563b85d2d3a5f
Altaf-Sheikh-Full-Stack <defaultcompny0@gmail.com>
2026-08-20 12:11:20 +0530

    Added the slach to the end of the url

--- vite-project/src/routes.ts
+++ vite-project/src/routes.ts
@@ -2,6 +2,6 @@ import { index, route, type RouteConfig } from '@react-router/dev/routes'
 
 export default [
   index('./Marketing/pages/Home/Home.tsx'),
-  route('pricing', './Marketing/pages/Pricing/Pricing.tsx'),
+  route('pricing/', './Marketing/pages/Pricing/Pricing.tsx'),
   route('*', './Marketing/pages/NotFound/NotFound.tsx'),
 ] satisfies RouteConfig
